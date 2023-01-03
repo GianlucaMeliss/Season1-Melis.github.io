1f2a69e7587260361ad786c8f32c4ce7f839706e
GianlucaMelis <gianluca.melis05@gmail.com>
2023-01-03 15:26:50 +0100

    fine starvation

--- pages/starvation/scrpitstarvation.js
+++ pages/starvation/scrpitstarvation.js
@@ -24,11 +24,11 @@ var t1 = "1 thread in esecuzione di massima priorita' <br> con 4 thread in coda 
 var t2 = "il thread precedente termina, e viene eseguito il thread successivo nella coda.";
 var t3 = "il thread precendente termina e viene eseguito il successivo per priorita', <br> nel mentre se ne aggiunge un'altro che per priorita' andra' in cima alla coda.";
 var t4 = "Il thread precedente termina, e viene eseguito il thread successivo nella coda. <br> anche se e' arrivato dopo per priorita' vera' eseguito prima.";
-var t5 = "Durante l'esecuzione del thread se ne sono aggiunti 2 (Arancione e Giallo) <br> in questo caso il thread arancione andra' in coma alla cosa <br> mentre il thread giallo si accodera' a quelli della sua stessa priorita'";
-var t6 = "";
-var t7 = "";
-var t8 = "";
-var t9 = "";
+var t5 = "Durante l'esecuzione del thread se ne sono aggiunti 2 (Arancione e Giallo) <br> in questo caso il thread arancione andra' in cima alla coda <br> mentre il thread giallo si accodera' a quelli della sua stessa priorita',<br> prima pero' del thread verde.";
+var t6 = "La coda procede secondo l'ordine di priorita'.";
+var t7 = "La coda procede secondo l'ordine di priorita'.";
+var t8 = "La coda procede secondo l'ordine di priorita'.";
+var t9 = "La coda procede secondo l'ordine di priorita'; <br> il thread di priorita' verde anziche' essere eseguito per quarto viene eseguito come settimo. <br> In questo caso la situazione di starvation si e' risolta in modo autonomo,<br> ma potrebbe accadere che continuino ad arrivare thread con priorita' maggiore senza poter quindi mai essere eseguito";
 
 function gestioneTesto() {
     switch (contaImg) {
